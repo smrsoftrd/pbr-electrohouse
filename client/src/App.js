aa64fd34ba782fd0779b8dfa05186831496c63ae
socratesmedina <socrates.medina.rosa@gmail.com>
2019-06-20 14:48:41 -0400

    Missing basics mods for client side

--- client/src/App.js
+++ client/src/App.js
@@ -7,8 +7,8 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+        <p>          
+          <br/><br/>Sistema de Gestión de Inventario
         </p>
         <a
           className="App-link"
